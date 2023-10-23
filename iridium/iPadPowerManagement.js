0f3829174f565079a9f8d3c4fb5c84f0cbb90bdb
marssys <gera.petrov@gmail.com>
2023-10-23 23:47:09 +0300

    Update iPadPowerManagement.js

--- iridium/iPadPowerManagement.js
+++ iridium/iPadPowerManagement.js
@@ -9,13 +9,16 @@ IR.AddListener(IR.EVENT_WORK, 0, function(time)
     {
         timer = 0;
         batteryLevel = IR.GetVariable("System.Battery.Level");
+        IR.Log(batteryLevel);
         if(batteryLevel > 90)
         {
             IR.GetDevice('KNX IP BAOS 774').Set("Питание iPad Вкл/Выкл", 0);
+            IR.Log('iPad Power OFF');
         }
         if(batteryLevel < 10)
         {
             IR.GetDevice('KNX IP BAOS 774').Set("Питание iPad Вкл/Выкл", 1);
+            IR.Log('iPad Power ON');
         } 
     }
 });
